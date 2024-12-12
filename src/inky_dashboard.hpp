f1b63dfd6c2d5350d6f1ab237980703e9a78fb96
Jaeheon Shim <git@jaeheonshim.com>
2024-12-12 01:05:53 -0500

    Add inky dashboard template file

---
+++ src/inky_dashboard.hpp
@@ -0,0 +1,8 @@
+inline constexpr int STATUS_LED = 6;
+inline constexpr int WIFI_LED = 7;
+
+inline const char *SSID = "<ssid>";
+inline const char *PASS = "<pass>";
+
+inline const char *HOST = "<server endpoint>";
+inline const int PORT   = 80;
