1da87c2ec07538a741577faec21f3029df60e3c2
Jaeheon Shim <git@jaeheonshim.com>
2025-01-05 21:59:28 -0500

    Remove unused fonts

--- lv_conf.h
+++ lv_conf.h
@@ -577,13 +577,11 @@
  *  @endcode
  */
 #define LV_FONT_CUSTOM_DECLARE  \
-LV_FONT_DECLARE(roboto_light_10) \
 LV_FONT_DECLARE(roboto_light_12) \
 LV_FONT_DECLARE(roboto_regular_12) \
 LV_FONT_DECLARE(roboto_regular_14) \
 LV_FONT_DECLARE(roboto_regular_18) \
 LV_FONT_DECLARE(roboto_regular_20) \
-LV_FONT_DECLARE(roboto_medium_14) \
 LV_FONT_DECLARE(roboto_medium_16) \
 LV_FONT_DECLARE(roboto_bold_14) \
 LV_FONT_DECLARE(roboto_bold_30) \
